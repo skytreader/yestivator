cdf5bdb0a920b4957ccf9292e0ec5d2f8b7b937e
Chad Estioco <chadestioco@gmail.com>
2015-03-28 17:18:58 +0800

    Main script.

--- yestivator.py
+++ yestivator.py
@@ -1,5 +1,7 @@
 import json
+import pynotify
 import random
+import time
 
 class QuoteSource(object):
     """
@@ -11,3 +13,20 @@ class QuoteSource(object):
 
     def get_quote(self):
         return choice.random(self.quotes)
+
+class Yestivator(object):
+    
+    def __init__(self, quote_source, freq):
+        self.quote_source = quote_source
+        self.freq = freq
+        pynotify.init("Basic")
+
+    def run(self):
+        time.sleep(self.freq)
+        notif = pynotify.Notification("YES", self.quote_source.get_quote())
+        notif.show()
+
+if __name__ == "__main__":
+    quotes = QuoteSource()
+    yessarian = Yestivator(quotes, 8)
+    yessarian.run()
